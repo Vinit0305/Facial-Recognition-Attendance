7da059fff27002333bc1d60e86716a61a2ed894e
Vinit Patel <vinitpatel353@gmail.com>
2024-07-18 19:58:54 +0530

    Final Commit

---
+++ Create_dataset.py
@@ -0,0 +1,72 @@
+import cv2
+import os
+
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+face_id = input('enter your id ')
+# Start capturing video
+vid_cam = cv2.VideoCapture(0)
+if not vid_cam.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+
+# Detect object in video stream using Haarcascade Frontal Face
+face_detector = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')
+if face_detector.empty():
+    print("Error: Cascade Classifier not loaded!")
+    exit()
+
+# Initialize sample face image
+count = 0
+
+assure_path_exists(os.path.join("dataset", "User"))
+
+
+# Start looping
+while True:
+    # Capture video frame
+    _, image_frame = vid_cam.read()
+
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect frames of different sizes, list of faces rectangles
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    # Loops for each faces
+    for (x, y, w, h) in faces:
+        # Crop the image frame into rectangle
+        cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Save the captured image into the datasets folder
+        cv2.imwrite(os.path.join("dataset", "User", "User." + str(face_id) + '.' + str(count) + ".jpg"), gray[y:y + h, x:x + w])
+
+    # Display the video frame, with bounded rectangle on the person's face
+    cv2.imshow('frame', image_frame)
+    
+
+    # Increment sample face image
+    count += len(faces)
+    print(count)
+
+    # To stop taking video, press 'q' for at least 100ms
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+    # If image taken reaches 10, stop taking video
+    elif count >= 50:
+        print("Successfully Captured")
+        break
+
+# Stop video
+vid_cam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
